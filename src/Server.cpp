1c5488b9c4dfdce621a736dd6d68d46f081d3b2b
Idhibhat Pankam <marginpankam@gmail.com>
2025-03-18 23:54:10 +0700

    refactor init

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include "./commands/init.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -23,31 +24,7 @@ int main(int argc, char *argv[])
 
     if (command == "init")
     {
-        try
-        {
-            std::filesystem::create_directory(".git");
-            std::filesystem::create_directory(".git/objects");
-            std::filesystem::create_directory(".git/refs");
-
-            std::ofstream headFile(".git/HEAD");
-            if (headFile.is_open())
-            {
-                headFile << "ref: refs/heads/main\n";
-                headFile.close();
-            }
-            else
-            {
-                std::cerr << "Failed to create .git/HEAD file.\n";
-                return EXIT_FAILURE;
-            }
-
-            std::cout << "Initialized git directory\n";
-        }
-        catch (const std::filesystem::filesystem_error &e)
-        {
-            std::cerr << e.what() << '\n';
-            return EXIT_FAILURE;
-        }
+        initialize_git();
     }
     else
     {
---
+++ src/commands/init.hpp
@@ -0,0 +1,10 @@
+#ifndef GIT_INIT_H
+#define GIT_INIT_H
+
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+
+void initialize_git();
+
+#endif // GIT_INIT_H
